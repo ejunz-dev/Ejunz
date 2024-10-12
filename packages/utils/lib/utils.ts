54cc341dc8be2ac889e54668049eaa4f38792537
Jacka10086 <496806255@qq.com>
2024-10-12 21:25:13 +0800

    errorMessage

---
+++ packages/utils/lib/utils.ts
@@ -0,0 +1,30 @@
+export function errorMessage(err: Error | string) {
+    const t = typeof err === 'string' ? err : err.stack;
+    const lines = t.split('\n')
+        .filter((i) => !i.includes(' (node:') && !i.includes('(internal'));
+    let cursor = 1;
+    let count = 0;
+    while (cursor < lines.length) {
+        if (lines[cursor] !== lines[cursor - 1]) {
+            if (count) {
+                lines[cursor - 1] += ` [+${count}]`;
+                count = 0;
+            }
+            cursor++;
+        } else {
+            count++;
+            lines.splice(cursor, 1);
+        }
+    }
+    const parsed = lines.join('\n')
+        .replace(/[A-Z]:\\.+\\@ejunz\\/g, '@ejunz\\')
+        .replace(/\/.+\/@ejunz\//g, '\\')
+        .replace(/[A-Z]:\\.+\\ejun\\/g, 'ejun\\')
+        .replace(/\/.+\/ejun\//g, 'ejun/')
+        .replace(/[A-Z]:\\.+\\node_modules\\/g, '')
+        .replace(/\/.+\/node_modules\//g, '')
+        .replace(/\\/g, '/');
+    if (typeof err === 'string') return parsed;
+    err.stack = parsed;
+    return err;
+}
