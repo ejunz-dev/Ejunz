7df02f6fe340730ce940c685f53dbc21f3f6468d
Jacka10086 <496806255@qq.com>
2024-11-25 11:51:39 +0800

    addon/blog-injectUI-userdetail\done

--- packages/ejun/src/interface.ts
+++ packages/ejun/src/interface.ts
@@ -787,7 +787,7 @@ export interface Lib extends Record<string, any> {
     problemSearch: ProblemSearch;
 }
 
-export type UIInjectableFields = 'ProblemAdd' | 'Notification' | 'Nav' | 'UserDropdown' | 'DomainManage' | 'ControlPanel';
+export type UIInjectableFields = 'ProblemAdd' | 'Notification' | 'Nav' | 'UserDropdown' | 'DomainManage' | 'ControlPanel' | 'ProfileHeaderContact';
 export interface UI {
     template: Record<string, string>,
     nodes: Record<UIInjectableFields, any[]>,
