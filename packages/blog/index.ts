8a5ca924bb164192bb57fe4aac6799da0b6a918a
Jacka10086 <496806255@qq.com>
2024-11-27 06:37:59 +0800

    fix/addon_blog-i18n_perm\done

--- packages/blog/index.ts
+++ packages/blog/index.ts
@@ -248,32 +248,45 @@ export async function apply(ctx: Context) {
         
     }
 ));
-
     // 加载多语言支持
     ctx.i18n.load('zh', {
         "{0}'s blog": '{0} 的博客',
         Blog: '博客',
+        blog: '博客',
         blog_detail: '博客详情',
         blog_edit: '编辑博客',
         blog_main: '博客',
+        'View blogs': '查看博客',
+        'View detailed blogs': '查看详细博客',
     });
     ctx.i18n.load('zh_TW', {
         "{0}'s blog": '{0} 的部落格',
         Blog: '部落格',
+        blog: '部落格',
         blog_detail: '部落格詳情',
         blog_edit: '編輯部落格',
         blog_main: '部落格',
+        'View blogs': '檢視博客',
+        'View detailed blogs': '檢視詳細博客',
     });
     ctx.i18n.load('kr', {
         "{0}'s blog": '{0}의 블로그',
         Blog: '블로그',
+        blog: '블로그',
         blog_main: '블로그',
         blog_detail: '블로그 상세',
         blog_edit: '블로그 수정',
+        'View blogs': '블로그 보기',
+        'View detailed blogs': '상세 블로그 보기',
     });
     ctx.i18n.load('en', {
+        blog: 'Blog',
         blog_main: 'Blog',
         blog_detail: 'Blog Detail',
         blog_edit: 'Edit Blog',
+        'View blogs': 'View blogs',
+        'View detailed blogs': 'View detailed blogs',
     });
+
+
 }
