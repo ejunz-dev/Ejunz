bd35c54ac471e0a6dec8cf89f9343154f8972411
Jacka10086 <496806255@qq.com>
2024-12-31 22:57:11 +0800

    init/Domain_Library

--- packages/ejunzlibrary/index.ts
+++ packages/ejunzlibrary/index.ts
@@ -1,13 +1,14 @@
 import {
     _, Context, DiscussionNotFoundError, DocumentModel, Filter,
     Handler, NumberKeys, ObjectId, OplogModel, paginate,
-    param, PRIV, Types, UserModel,
+    param, PRIV, Types, UserModel, DomainModel
 } from 'ejun';
 
 export const TYPE_LIBRARY: 100 = 100;
 export interface LibraryDoc {
     docType: 100;
     docId: ObjectId;
+    domainId: string,
     owner: number;
     title: string;
     content: string;
@@ -29,9 +30,10 @@ declare module 'ejun' {
 
 export class LibraryModel {
     static async add(
-        owner: number, title: string, content: string, ip?: string,
+        domainId:string, owner: number, title: string, content: string, ip?: string,
     ): Promise<ObjectId> {
         const payload: Partial<LibraryDoc> = {
+            domainId,
             content,
             owner,
             title,
@@ -41,60 +43,60 @@ export class LibraryModel {
             views: 0,
         };
         const res = await DocumentModel.add(
-            'system', payload.content!, payload.owner!, TYPE_LIBRARY,
+            domainId, payload.content!, payload.owner!, TYPE_LIBRARY,
             null, null, null, _.omit(payload, ['domainId', 'content', 'owner']),
         );
         payload.docId = res;
         return payload.docId;
     }
 
-    static async get(did: ObjectId): Promise<LibraryDoc> {
-        return await DocumentModel.get('system', TYPE_LIBRARY, did);
+    static async get(domainId: string, did: ObjectId): Promise<LibraryDoc> {
+        return await DocumentModel.get(domainId, TYPE_LIBRARY, did);
     }
 
-    static edit(did: ObjectId, title: string, content: string): Promise<LibraryDoc> {
+    static edit(domainId: string, did: ObjectId, title: string, content: string): Promise<LibraryDoc> {
         const payload = { title, content };
-        return DocumentModel.set('system', TYPE_LIBRARY, did, payload);
+        return DocumentModel.set(domainId, TYPE_LIBRARY, did, payload);
     }
 
-    static inc(did: ObjectId, key: NumberKeys<LibraryDoc>, value: number): Promise<LibraryDoc | null> {
-        return DocumentModel.inc('system', TYPE_LIBRARY, did, key, value);
+    static inc(domainId: string, did: ObjectId, key: NumberKeys<LibraryDoc>, value: number): Promise<LibraryDoc | null> {
+        return DocumentModel.inc(domainId, TYPE_LIBRARY, did, key, value);
     }
 
-    static del(did: ObjectId): Promise<never> {
+    static del(domainId: string, did: ObjectId): Promise<never> {
         return Promise.all([
-            DocumentModel.deleteOne('system', TYPE_LIBRARY, did),
-            DocumentModel.deleteMultiStatus('system', TYPE_LIBRARY, { docId: did }),
+            DocumentModel.deleteOne(domainId, TYPE_LIBRARY, did),
+            DocumentModel.deleteMultiStatus(domainId, TYPE_LIBRARY, { docId: did }),
         ]) as any;
     }
 
-    static count(query: Filter<LibraryDoc>) {
-        return DocumentModel.count('system', TYPE_LIBRARY, query);
+    static count(domainId: string, query: Filter<LibraryDoc>) {
+        return DocumentModel.count(domainId, TYPE_LIBRARY, query);
     }
 
-    static getMulti(query: Filter<LibraryDoc> = {}) {
-        return DocumentModel.getMulti('system', TYPE_LIBRARY, query)
+    static getMulti(domainId: string, query: Filter<LibraryDoc> = {}) {
+        return DocumentModel.getMulti(domainId, TYPE_LIBRARY, query)
             .sort({ _id: -1 });
     }
 
-    static async addReply(did: ObjectId, owner: number, content: string, ip: string): Promise<ObjectId> {
+    static async addReply(domainId: string, did: ObjectId, owner: number, content: string, ip: string): Promise<ObjectId> {
         const [[, drid]] = await Promise.all([
-            DocumentModel.push('system', TYPE_LIBRARY, did, 'reply', content, owner, { ip }),
-            DocumentModel.incAndSet('system', TYPE_LIBRARY, did, 'nReply', 1, { updateAt: new Date() }),
+            DocumentModel.push(domainId, TYPE_LIBRARY, did, 'reply', content, owner, { ip }),
+            DocumentModel.incAndSet(domainId, TYPE_LIBRARY, did, 'nReply', 1, { updateAt: new Date() }),
         ]);
         return drid;
     }
 
-    static setStar(did: ObjectId, uid: number, star: boolean) {
-        return DocumentModel.setStatus('system', TYPE_LIBRARY, did, uid, { star });
+    static setStar(domainId: string, did: ObjectId, uid: number, star: boolean) {
+        return DocumentModel.setStatus(domainId, TYPE_LIBRARY, did, uid, { star });
     }
 
-    static getStatus(did: ObjectId, uid: number) {
-        return DocumentModel.getStatus('system', TYPE_LIBRARY, did, uid);
+    static getStatus(domainId: string, did: ObjectId, uid: number) {
+        return DocumentModel.getStatus(domainId, TYPE_LIBRARY, did, uid);
     }
 
-    static setStatus(did: ObjectId, uid: number, $set) {
-        return DocumentModel.setStatus('system', TYPE_LIBRARY, did, uid, $set);
+    static setStatus(domainId: string, did: ObjectId, uid: number, $set) {
+        return DocumentModel.setStatus(domainId, TYPE_LIBRARY, did, uid, $set);
     }
 }
 
@@ -106,43 +108,68 @@ class LibraryHandler extends Handler {
     @param('did', Types.ObjectId, true)
     async _prepare(domainId: string, did: ObjectId) {
         if (did) {
-            this.ddoc = await LibraryModel.get(did);
+            this.ddoc = await LibraryModel.get(domainId, did);
             if (!this.ddoc) throw new DiscussionNotFoundError(domainId, did);
         }
     }
 }
 
-class LibraryUserHandler extends LibraryHandler {
-    @param('uid', Types.Int)
-    @param('page', Types.PositiveInt, true)
-    async get(domainId: string, uid: number, page = 1) {
-        const [ddocs, dpcount] = await paginate(
-            LibraryModel.getMulti({ owner: uid }),
-            page,
-            10,
-        );
-        const udoc = await UserModel.getById(domainId, uid);
-        this.response.template = 'library_main.html';
-        this.response.body = {
-            ddocs,
-            dpcount,
-            udoc,
-            page,
-        };
+
+
+
+export class LibraryDomainHandler extends Handler {
+    async get({ domainId, page = 1, pageSize = 10 }) {
+        domainId = this.args?.domainId || this.context?.domainId || 'system';
+
+        const query = {};
+        try {
+            const domainInfo = await DomainModel.get(domainId);
+
+            if (!domainInfo) {
+                throw new Error(`Domain not found for id: ${domainId}`);
+            }
+
+            const [ddocs, totalPages, totalCount] = await paginate(
+                LibraryModel.getMulti(domainId, query),
+                page,
+                pageSize
+            );
+
+            this.response.template = 'library_domain.html';
+            this.response.body = {
+                ddocs,
+                domainId,
+                dname: domainInfo.name,
+                page,
+                pageSize,
+                totalPages,
+                totalCount,
+            };
+        } catch (error) {
+            this.response.template = 'error.html';
+            this.response.body = {
+                error: 'Failed to fetch documents for the domain.',
+            };
+        }
     }
 }
+
+
+
+
+
 
 class LibraryDetailHandler extends LibraryHandler {
     @param('did', Types.ObjectId)
     async get(domainId: string, did: ObjectId) {
         const dsdoc = this.user.hasPriv(PRIV.PRIV_USER_PROFILE)
-            ? await LibraryModel.getStatus(did, this.user._id)
+            ? await LibraryModel.getStatus(domainId, did, this.user._id)
             : null;
         const udoc = await UserModel.getById(domainId, this.ddoc!.owner);
         if (!dsdoc?.view) {
             await Promise.all([
-                LibraryModel.inc(did, 'views', 1),
-                LibraryModel.setStatus(did, this.user._id, { view: true }),
+                LibraryModel.inc(domainId, did, 'views', 1),
+                LibraryModel.setStatus(domainId, did, this.user._id, { view: true }),
             ]);
         }
         this.response.template = 'library_detail.html';
@@ -157,13 +184,13 @@ class LibraryDetailHandler extends LibraryHandler {
 
     @param('did', Types.ObjectId)
     async postStar(domainId: string, did: ObjectId) {
-        await LibraryModel.setStar(did, this.user._id, true);
+        await LibraryModel.setStar(domainId, did, this.user._id, true);
         this.back({ star: true });
     }
 
     @param('did', Types.ObjectId)
     async postUnstar(domainId: string, did: ObjectId) {
-        await LibraryModel.setStar(did, this.user._id, false);
+        await LibraryModel.setStar(domainId, did, this.user._id, false);
         this.back({ star: false });
     }
 }
@@ -178,7 +205,7 @@ class LibraryEditHandler extends LibraryHandler {
     @param('content', Types.Content)
     async postCreate(domainId: string, title: string, content: string) {
         await this.limitRate('add_library', 3600, 60);
-        const did = await LibraryModel.add(this.user._id, title, content, this.request.ip);
+        const did = await LibraryModel.add(domainId, this.user._id, title, content, this.request.ip);
         this.response.body = { did };
         this.response.redirect = this.url('library_detail', { uid: this.user._id, did });
     }
@@ -189,7 +216,7 @@ class LibraryEditHandler extends LibraryHandler {
     async postUpdate(domainId: string, did: ObjectId, title: string, content: string) {
         if (!this.user.own(this.ddoc!)) this.checkPriv(PRIV.PRIV_EDIT_SYSTEM);
         await Promise.all([
-            LibraryModel.edit(did, title, content),
+            LibraryModel.edit(domainId, did, title, content),
             OplogModel.log(this, 'library.edit', this.ddoc),
         ]);
         this.response.body = { did };
@@ -200,7 +227,7 @@ class LibraryEditHandler extends LibraryHandler {
     async postDelete(domainId: string, did: ObjectId) {
         if (!this.user.own(this.ddoc!)) this.checkPriv(PRIV.PRIV_EDIT_SYSTEM);
         await Promise.all([
-            LibraryModel.del(did),
+            LibraryModel.del(domainId, did),
             OplogModel.log(this, 'library.delete', this.ddoc),
         ]);
         this.response.redirect = this.url('library_main', { uid: this.ddoc!.owner });
@@ -208,12 +235,17 @@ class LibraryEditHandler extends LibraryHandler {
 }
 
 export async function apply(ctx: Context) {
-    ctx.Route('library_main', '/library/:uid', LibraryUserHandler);
-    ctx.Route('library_create', '/library/:uid/create', LibraryEditHandler, PRIV.PRIV_USER_PROFILE);
-    ctx.Route('library_detail', '/library/:uid/:did', LibraryDetailHandler);
-    ctx.Route('library_edit', '/library/:uid/:did/edit', LibraryEditHandler, PRIV.PRIV_USER_PROFILE);
-    ctx.injectUI('UserDropdown', 'library_main', (h) => ({ icon: 'book', displayName: 'Library', uid: h.user._id.toString() }),
-        PRIV.PRIV_USER_PROFILE);
+    // ctx.Route('library', '/library', LibHandler);
+    ctx.Route('library_domain', '/library', LibraryDomainHandler);
+    ctx.Route('library_create', '/library/create', LibraryEditHandler, PRIV.PRIV_USER_PROFILE);
+    ctx.Route('library_detail', '/library/:did', LibraryDetailHandler);
+    ctx.Route('library_edit', '/library/:did/edit', LibraryEditHandler, PRIV.PRIV_USER_PROFILE);
+    ctx.injectUI('Nav', 'library_domain', () => ({
+        name: 'library_domain',
+        displayName: 'Library',
+        args: {},
+        checker: (handler) => handler.user.hasPriv(PRIV.PRIV_USER_PROFILE),
+    }));
     ctx.i18n.load('zh', {
         "{0}'s library": '{0} 的项目',
         Library: '项目',
