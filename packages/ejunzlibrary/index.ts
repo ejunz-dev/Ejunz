7dec713d673dc0972173870a311f88801e585f4f
Jacka10086 <496806255@qq.com>
2025-01-07 05:29:29 +0800

    fix/ejunzlibraryUpdate_Del

--- packages/ejunzlibrary/index.ts
+++ packages/ejunzlibrary/index.ts
@@ -267,7 +267,6 @@ export class LibraryEditHandler extends LibraryHandler {
         const domainId = this.context.domainId || 'default_domain';
         const files = await StorageModel.list(`domain/${domainId}/`);
 
-        // 定义用于生成文件 URL 的函数
         const urlForFile = (filename: string) => `/d/${domainId}/domainfile/${encodeURIComponent(filename)}`;
 
         this.response.template = 'library_edit.html';
@@ -278,14 +277,11 @@ export class LibraryEditHandler extends LibraryHandler {
         };
     }
 
-
-
     @param('title', Types.Title)
     @param('content', Types.Content)
     async postCreate(domainId: string, title: string, content: string) {
         await this.limitRate('add_library', 3600, 60);
-        
-        // 调用 addWithId 方法生成并保存 lid
+
         const did = await LibraryModel.addWithId(
             domainId,
             this.user._id,
@@ -298,9 +294,30 @@ export class LibraryEditHandler extends LibraryHandler {
         this.response.redirect = this.url('library_detail', { uid: this.user._id, did });
     }
 
+    @param('did', Types.ObjectId)
+    @param('title', Types.Title)
+    @param('content', Types.Content)
+    async postUpdate(domainId: string, did: ObjectId, title: string, content: string) {
+       
+        await Promise.all([
+            LibraryModel.edit(domainId,did, title, content),
+            OplogModel.log(this, 'library.edit', this.ddoc),
+        ]);
 
+        this.response.body = { did };
+        this.response.redirect = this.url('library_detail', { uid: this.user._id, did });
+    }
 
-    
+    @param('did', Types.ObjectId)
+    async postDelete(domainId: string, did: ObjectId) {
+
+        await Promise.all([
+            LibraryModel.del(domainId, did),
+            OplogModel.log(this, 'library.delete', this.ddoc),
+        ]);
+
+        this.response.redirect = this.url('library_domain');
+    }
 }
 
 
