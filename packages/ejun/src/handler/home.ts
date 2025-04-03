044e0e14b5e63d72bfc088ac15d57c28aaf8ba4b
Jacka10086 <496806255@qq.com>
2025-04-04 00:20:58 +0800

    add Centers/html

--- packages/ejun/src/handler/home.ts
+++ packages/ejun/src/handler/home.ts
@@ -33,24 +33,6 @@ import {
 import { camelCase, md5 } from '../utils';
 
 
-export class GrowthBaseHandler extends Handler {
-    async after(domainId: string) {
-        this.response.body.overrideNav = [
-            {
-                name: 'homepage',
-                args: {},
-                displayName: 'Back to homepage',
-                checker: () => true,
-            },
-        ];
-    }
-}
-export class GrowthHandler extends GrowthBaseHandler {
-    async get() {
-        this.response.template = 'growth_main.html';
-    }
-}
-
 export class HomeHandler extends Handler {
     uids = new Set<number>();
 
@@ -625,7 +607,6 @@ export async function apply(ctx: Context) {
         geoip = g.geoip;
     });
     ctx.Route('homepage', '/', HomeHandler);
-    ctx.Route('growth_main', '/growth', GrowthHandler);
     ctx.Route('home_security', '/home/security', HomeSecurityHandler, PRIV.PRIV_USER_PROFILE);
     ctx.Route('user_changemail_with_code', '/home/changeMail/:code', UserChangemailWithCodeHandler, PRIV.PRIV_USER_PROFILE);
     ctx.Route('home_settings', '/home/settings/:category', HomeSettingsHandler, PRIV.PRIV_USER_PROFILE);
