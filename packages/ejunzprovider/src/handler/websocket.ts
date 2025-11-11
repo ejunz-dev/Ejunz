d98839e5732ecb8b97cf826ded27a599f5dd35a1
Jacka10086 <496806255@qq.com>
2025-11-11 22:10:41 +0800

    clean: delete packages

--- packages/ejunzprovider/src/handler/websocket.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'ejun';
-import { handleWebSocket } from './logs';
-
-export async function apply(ctx: Context) {
-    ctx.server?.router.ws('/mcp/ws', (socket, request, ctx) => {
-        handleWebSocket(socket);
-    });
-}
