c5bfc02c8bde61b3408d0852c36f9437f8059201
Jacka10086 <496806255@qq.com>
2026-02-25 01:51:26 +0800

    ui: add cardFace edit and display

--- packages/ejun/src/model/base.ts
+++ packages/ejun/src/model/base.ts
@@ -866,7 +866,7 @@ export class CardModel {
     static async update(
         domainId: string,
         docId: ObjectId,
-        updates: Partial<Pick<CardDoc, 'title' | 'content' | 'order' | 'nodeId' | 'problems'>>
+        updates: Partial<Pick<CardDoc, 'title' | 'content' | 'cardFace' | 'order' | 'nodeId' | 'problems'>>
     ): Promise<void> {
         await document.set(domainId, TYPE_CARD, docId, {
             ...updates,
