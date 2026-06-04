722d23924b84f8f6286de7d96f435d289ab7540c
Jacka10086 <496806255@qq.com>
2026-06-04 23:46:26 +0800

    core&ui: add pluggable edge types

--- packages/ejun/src/model/edge_token.ts
+++ packages/ejun/src/model/edge_token.ts
@@ -6,7 +6,7 @@ import { randomstring } from '../utils';
 export interface EdgeTokenDoc {
     _id: ObjectId;
     domainId: string;
-    type: 'provider' | 'node' | 'client' | 'repo';
+    type: string;
     token: string;
     owner: number; // 创建 token 的用户 ID
     lastUsedAt: Date;
@@ -23,7 +23,7 @@ class EdgeTokenModel {
 
     static async add(
         domainId: string,
-        type: 'provider' | 'node' | 'client' | 'repo',
+        type: string,
         token: string,
         owner: number,
     ): Promise<EdgeTokenDoc> {
